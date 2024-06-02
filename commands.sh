a36eb1e37ac72787c096f0eedd7c8a71a1887448
mayur jadhav <maur.r.jadhav@gmail.com>
2024-06-02 16:20:39 +0530

    added new commands with terraform script to create the ec2/vm with docker installed

--- commands.sh
+++ commands.sh
@@ -8,13 +8,18 @@ sudo usermod -aG docker <'user-name'>     # this will add your current user to t
 
 docker container run <'image_name'>
 
-docker container run -d -P --name <'container-name'> <'image_name'>     # run the container 
-
-docker container exec -it <'container-name'> /bin/bash          # entering into the container       
-                                                                # -it stands for interactive terminal
-
 docker container ls -a / --all          # list all containers
 
-docker images --all             # list all images
 
+# docker image creation
 docker image build -t <image-name>:<tag> .
+docker images --all             # list all images
+docker image rm -f $(docker image ls -q)        # delete the all images forcefully
+
+
+# container rnn
+docker container run -d -P --name <'container-name'> <'image_name'>    # dynamic container port forwardeing
+docker container run -d -p --name <'container-name'> <'image_name'>     # static container port forwardeing
+docker container exec -it <'container-name'> <'shell'>          # entering into the container       
+                                                                # -it stands for interactive terminal
+docker container rm -f $(docker container ls -a -q)         # delete all container at a time
